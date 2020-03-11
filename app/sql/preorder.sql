926ce5def63c97080267d33e9904e1a78f166759
itsmindyk <mindy.keng.2018@smu.edu.sg>
2020-03-11 18:23:07 +0800

    DATABASE DATA NAME STANDARDISATION

--- app/sql/preorder.sql
+++ app/sql/preorder.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 06, 2020 at 05:27 PM
+-- Generation Time: Mar 11, 2020 at 10:21 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -12,17 +12,14 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-DROP DATABASE IF EXISTS preorder;
 
-CREATE DATABASE preorder;
-USE preorder;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `travel_postman`
+-- Database: `preorder`
 --
 
 -- --------------------------------------------------------
@@ -33,12 +30,12 @@ USE preorder;
 
 DROP TABLE IF EXISTS `preorder_details`;
 CREATE TABLE IF NOT EXISTS `preorder_details` (
-  `poid` varchar(10) CHARACTER SET utf8 NOT NULL,
-  `travellerid` varchar(10) CHARACTER SET utf8 NOT NULL,
+  `po_id` varchar(10) CHARACTER SET utf8 NOT NULL,
+  `traveller_id` varchar(10) CHARACTER SET utf8 NOT NULL,
   `country` varchar(80) CHARACTER SET utf8 NOT NULL,
   `end_date` date NOT NULL,
-  `item_name` varchar(80) CHARACTER SET utf8 NOT NULL,
-  `item_category` varchar(80) CHARACTER SET utf8 NOT NULL,
+  `item_name` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `item_category` varchar(30) CHARACTER SET utf8 NOT NULL,
   `price` decimal(8,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -50,22 +47,11 @@ CREATE TABLE IF NOT EXISTS `preorder_details` (
 
 DROP TABLE IF EXISTS `registered_users`;
 CREATE TABLE IF NOT EXISTS `registered_users` (
-  `poid` varchar(10) CHARACTER SET utf8 DEFAULT NULL,
+  `po_id` varchar(10) CHARACTER SET utf8 DEFAULT NULL,
   `requester_id` varchar(10) CHARACTER SET utf8 DEFAULT NULL,
   `paid_status` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
-
-
--- --
--- -- Dumping data for table `user`
--- --
-
--- INSERT INTO `user` (`userid`, `name`, `travellerid`, `requesterid`, `password`, `email`) VALUES
--- ('100', 'Mister Postman', '200', '300', '123', 'postman@email.com');
--- COMMIT;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- app/sql/users.sql
+++ app/sql/users.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 11, 2020 at 09:49 AM
--- Server version: 5.7.19
--- PHP Version: 7.1.9
+-- Generation Time: Mar 11, 2020 at 10:22 AM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,8 +30,8 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `user_details`;
 CREATE TABLE IF NOT EXISTS `user_details` (
-  `user_id` int(27) NOT NULL,
-  `name` char(255) CHARACTER SET utf8 NOT NULL
+  `user_id` int(30) NOT NULL,
+  `name` varchar(80) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 COMMIT;
 
