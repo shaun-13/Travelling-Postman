43603497d0c336dd428913a895a26486759e7e03
shaun-13 <shaun.phua.2018@sis.smu.edu.sg>
2020-03-11 21:54:11 +0800

    Created function to retrieve preorders by poid

--- app/PreOrder.py
+++ app/PreOrder.py
@@ -11,51 +11,60 @@ class PreOrder(db.Model):
     __tablename__ = 'preorder_details'
  
     po_id = db.Column(db.String(10), primary_key=True)
-    traveller_id = db.Column(db.String(10), unique=True, nullable=False)
+    traveller_id = db.Column(db.String(32), unique=True, nullable=False)
     country = db.Column(db.String(30), nullable=False)
     end_date = db.Column(db.Date, nullable=False)
-    item_name = db.Column(db.String(30), nullable=False)
-    item_catagory = db.Column(db.String(30), nullable=False)
+    item_name = db.Column(db.String(32), nullable=False)
+    item_category = db.Column(db.String(32), nullable=False)
     price = db.Column(db.Float(precision=2), nullable=False)
  
-    def __init__(self, po_id, traveller_id, country, end_date, item_name, item_catagory, price):
+    def __init__(self, po_id, traveller_id, country, end_date, item_name, item_category, price):
         self.po_id = po_id
         self.traveller_id = traveller_id
         self.country = country
         self.end_date = end_date
         self.item_name = item_name
-        self.item_catagory = item_catagory
+        self.item_category = item_category
         self.price = price
  
     def json(self):
-        return {"po_id": self.po_id, "traveller_id": self.traveller_id, "country": self.country, "end_date": self.end_date, "item_name": self.item_name, "item_catagory": self.item_catagory, "price": self.price}
+        return {"po_id": self.po_id, "traveller_id": self.traveller_id, "country": self.country, "end_date": self.end_date, "item_name": self.item_name, "item_category": self.item_category, "price": self.price}
  
 class Registered(db.Model):
     __tablename__ = 'registered_users'
  
-    po_id = db.Column(db.String(10), nullable=False)
-    traveller_id = db.Column(db.String(10), nullable=False)
-    paid_status = db.Column(db.Boolean, default=False, nullable=False)
+    po_id = db.Column(db.String(10), primary_key=True, nullable=False)
+    requester_id = db.Column(db.String(32), primary_key=True, nullable=False)
  
-    def __init__(self, po_id, traveller_id, paid_status):
+    def __init__(self, po_id, requester_id):
         self.po_id = po_id
-        self.traveller_id = traveller_id
-        self.paid_status = paid_status
+        self.requester_id = requester_id
  
     def json(self):
         return {"po_id": self.po_id, "traveller_id": self.traveller_id, "paid_status": self.paid_status}
 
-@app.route("/preorders")
-def get_all():
+@app.route("/allPreOrders")
+def get_all_preOrders():
     return jsonify({"preorders": [preorder_details.json() for preorder_details in PreOrder.query.all()]})
 
-@app.route("/Registered_Users")
-def get_all():
-    return jsonify({"registered users": [registered_users.json() for registered_users in Registered.query.all()]})
+# @app.route("/Registered_Users")
+# def get_all():
+#     return jsonify({"registered users": [registered_users.json() for registered_users in Registered.query.all()]})
 
 
 #Function for POSTING preorder joined details to payment MS
 
 
 #Function for traveller creating PreOrder [POST]
-@
+
+
+@app.route("/preOrder/<string:po_id>")
+def find_by_poid(po_id):
+    preorder = PreOrder.query.filter_by(po_id=po_id).first()
+    if preorder:
+        return jsonify(preorder.json())
+    return jsonify({'message': 'Pre-Order not found'}), 404
+
+
+if __name__=='__main__':
+    app.run(port=5000, debug=True)
