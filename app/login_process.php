0dcbff537f050d7b2ac787e30dd72e56eb595107
jiayinggoh <jiaying.goh.2018@sis.smu.edu.sg>
2020-03-11 19:13:38 +0800

    edited login codes

--- app/login_process.php
+++ app/login_process.php
@@ -36,30 +36,32 @@
 
  $email = $_POST["email"];
  $password = $_POST["password"];
- $user_type = $_POST["user-type"];
+// $user_type = $_POST["user-type"];
  
  if (empty($email) || empty($password)) {
      echo '<h1>No user ID or password</h1>';
     exit;
  }
 
- elseif (empty($user_type)) {
-     echo '<h1>User type not selected!</h1>';
- }
+//  elseif (empty($user_type)) {
+//      echo '<h1>User type not selected!</h1>';
+//  }
 
  else {
     # validate userid and password
-    if($user_type == 'requester' && $email == 'requester@smu.com' && $password == '123') {
-        header ('Location: ../requester.html');
+    if(//$user_type == 'requester' && 
+    $email == 'requester@smu.com' && $password == '123' ) {
+        header ('Location: requester.html');
         exit;
     }
-    elseif($user_type == 'traveller' && $email == 'traveller@smu.com' && $password == '123') {
-        header ('Location: ../traveller.html');
+    elseif(//$user_type == 'traveller' && 
+    $email == 'traveller@smu.com' && $password == '123') {
+        header ('Location: traveller.html');
         exit;
     }
     else {
         echo '<h1>Login Failed. Please try again!</h1>';
-        echo '<a href="../login.html">Back to login page</a>';
+        echo '<a href="login.html">Back to login page</a>';
     }
  }
 
