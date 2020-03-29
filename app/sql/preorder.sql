25698612f2c4742bdda76b1e914cb45d1cf9e461
itsmindyk <mindy.keng.2018@smu.edu.sg>
2020-03-30 00:18:24 +0800

    Update preorder.sql

--- app/sql/preorder.sql
+++ app/sql/preorder.sql
@@ -1,13 +1,9 @@
-DROP DATABASE IF EXISTS preorder;
-CREATE DATABASE IF NOT EXISTS preorder;
-USE preorder;
-
 -- phpMyAdmin SQL Dump
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 22, 2020 at 04:04 PM
+-- Generation Time: Mar 29, 2020 at 04:17 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -34,14 +30,15 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `preorder_details`;
 CREATE TABLE IF NOT EXISTS `preorder_details` (
-  `po_id` int(10) NOT NULL,
+  `po_id` int(10) NOT NULL AUTO_INCREMENT,
   `traveller_id` varchar(10) CHARACTER SET utf8 NOT NULL,
   `country` varchar(80) CHARACTER SET utf8 NOT NULL,
   `end_date` date NOT NULL,
   `item_name` varchar(30) CHARACTER SET utf8 NOT NULL,
   `item_category` varchar(30) CHARACTER SET utf8 NOT NULL,
-  `price` decimal(8,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `price` decimal(8,2) NOT NULL,
+  PRIMARY KEY (`po_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `preorder_details`
@@ -49,8 +46,10 @@ CREATE TABLE IF NOT EXISTS `preorder_details` (
 
 INSERT INTO `preorder_details` (`po_id`, `traveller_id`, `country`, `end_date`, `item_name`, `item_category`, `price`) VALUES
 (1, '333', 'Iceland', '2019-01-01', 'chinese bowl', 'food', '23.20'),
-(2, '001', 'Singapore', '2020-03-31', 'Witch Broom', 'Witch Crafts', '99.30'),
-(3, '002', 'Malaysia', '2020-03-27', 'Angsty', 'Cakes', '7.23');
+(11, '333', 'Norway', '2020-05-01', 'Salomon', 'fish', '44.50'),
+(12, '333', 'India', '2020-04-01', 'Gods', 'statue', '10.45'),
+(55, '100', 'brazil', '2020-01-11', 'hair', 'clothes', '23.40');
+
 -- --------------------------------------------------------
 
 --
