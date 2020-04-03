37bb1518c7dad71954110c8034cc746c7fd81647
shaun-13 <shaun.phua.2018@sis.smu.edu.sg>
2020-04-03 14:49:00 +0800

    Corrected sql schema name

--- app/confirmedOrders2.py
+++ app/confirmedOrders2.py
@@ -11,7 +11,7 @@ import requests
 import pika
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/notifications'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/confirmed_order'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
